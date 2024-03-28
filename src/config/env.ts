063989f1ecfed58f181ae99efe9542707de39e0a
anthonymfv <tiet.xuan.sang@moneyfoward.vn>
2024-03-28 10:46:25 +0700

    slack and ci integration

--- src/config/env.ts
+++
@@ -1,8 +0,0 @@
-export const env = {
-  baseURL: "https://test.si.mfvn.dev/home",
-  account: {
-    username: 'duong.huynh.diem.mi+1@moneyforward.vn',
-    password: 'asdfasdfasdf',
-  },
-  authFile: 'src/.auth/user.json'
-};  
